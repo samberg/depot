3cb5e7efd21022b5cecd3a2c0eb5c8fa38bb474d
samberg <sam@clabs.org>
2016-02-21 22:11:00 -0600

    Product 0.0.2
    rake test has been fixed. Need to work through 9 errors, all of which
    deal with the decimal column in the products table. My guess is the
    column needs to be added or "removed".

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require 'test_helper.rb'
 
 class StoreControllerTest < ActionController::TestCase
   test "should get index" do
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,8 +1,16 @@
-test 'product attributes must not be empty' do
-  product = Product.new
-  assert product.invalid?
-  assert product.errors[:title].any?
-  assert product.errors[:description].any?
-  assert product.errors[:price].any?
-  assert product.errors[:image_url].any?
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  # test "the truth" do
+  #   assert true
+  # end
 end
