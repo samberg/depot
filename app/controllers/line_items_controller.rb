b7d01cdc85109d490092c0ff20e2c9ab981bf8f4
samberg <sam@clabs.org>
2016-03-01 20:18:05 -0600

    Product 0.0.6
    Missing a lot of methods.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,11 +27,11 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.add_product(product.id)
+    @line_item = @cart.line_items.build(product: product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,5 +8,9 @@ def add_product(product_id)
     current_item  = line_items.build(product_id: product_id)
   end
   current_item
+
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
 end
 end
