e4d59e5b50752789f5a12c621fc2ab8a64659b71
samberg <sam@clabs.org>
2016-03-03 21:31:20 -0600

    Product 0.0.7
    Store now features rendered Cart on left-hand side of site. Still lots
    of missing methods to deal with, as well as some partial_paths(?) to be
    dealing with.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,16 +1,17 @@
 class Cart < ActiveRecord::Base
-  has_many  :line_items, dependent:   :destroy
-def add_product(product_id)
-  current_item = line_items.find_by(product_id: product_id)
-  if current_item
-    current_item.quantity +=  1
-  else
-    current_item  = line_items.build(product_id: product_id)
-  end
-  current_item
+  has_many :line_item, dependent: :destroy
 
-  def total_price
-    line_items.to_a.sum { |item| item.total_price }
+  def add_product(product_id)
+    current_item = line_item.find_by(product_id: product_id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_item.build(product_id: product_id)
+    end
+    current_item
+
   end
-end
+  def total_price
+    line_item.to_a.sum { |item| item.total_price }
+  end
 end
